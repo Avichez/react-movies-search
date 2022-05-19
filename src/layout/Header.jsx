c8b8498b49b5f040f8c2962529e730be41f55012
Avichez <starkavichi@gmail.com>
2022-05-19 15:46:50 +0300

    added link to my Repository

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -4,9 +4,9 @@ const Header = () => {
     return (
         <nav className="grey darken-3">
             <div className="nav-wrapper">
-                <a href="!#" className="brand-logo center">Movies</a>
+                <a href="#" className="brand-logo center">Movies</a>
                 <ul id="nav-mobile" className="left hide-on-med-and-down">
-                    <li><a href="!#">Repository</a></li>
+                    <li><a href="https://github.com/Avichez/react-movies-search">Repository</a></li>
                 </ul>
             </div>
         </nav>
